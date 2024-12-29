f0bb707ea673df05e9b7236b7082745a3f5e41a5
gorshinsk <gorshinskiyy@gmail.com>
2024-12-30 01:17:47 +0200

    AdminLTE Initialized

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\Main\IndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', \App\Http\Controllers\IndexController::class)->name('main.index');
