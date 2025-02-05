89bd33ba9069894460989c0b65c627fa7386c77a
gorshinsk <gorshinskiyy@gmail.com>
2025-02-05 10:36:32 +0200

    nnn

---
+++ app/Http/Controllers/API/Product/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\API\Product;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function __invoke()
+    {
+        return 11111111;
+    }
+}
